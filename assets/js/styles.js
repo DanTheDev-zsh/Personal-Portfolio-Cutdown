68eb13d34c4a7213a01d3cf0c07aecbe8b3721b6
DanTheDev-zsh <XuDanDev@gmail.com>
2022-06-23 19:27:37 -0700

    added most css styles, still have bugged rules need to fix

---
+++ assets/js/styles.js
@@ -0,0 +1,4 @@
+// sike, no javascript here...
+
+// but I wrote some js on the original repo.
+// I implimented a function to copy attribute value from element to clipboard, while telling user in a tooltip the process is successful
